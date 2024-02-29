2d90ea7c24245d28f6a3d7f74f53c982e0c114bb
prashannraj <prashannraj@hotmail.com>
2024-02-29 13:12:10 +0545

    password hashing in userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 //schema
 
@@ -29,5 +30,9 @@ const userSchema = new mongoose.Schema({
 },
 {timestamps: true}
 );
-
+// middleware for password hash
+userSchema.pre('save', async function() {
+        const salt =await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt);
+})
 export default mongoose.model('User', userSchema)
