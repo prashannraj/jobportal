0c34ee68cecd9f3a7c8b949f0a41272d76f5df9b
prashannraj <prashannraj@hotmail.com>
2024-03-03 14:02:37 +0545

    two stpe authentication setup failed, can not read propertise of undefined (reading 'split')

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import {testPostController} from '../controllers/testController.js';
+import userAuth from '../middelwares/authmiddleware.js';
 
 //route object
 
 const router = express.Router();
 
 //routes
-router.post("/testPost", testPostController)
+router.post("/testPost", userAuth, testPostController)
 
 //export
 export default router
